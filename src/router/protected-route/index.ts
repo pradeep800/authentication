b458507893d34dfc2232dfa3bbe29554c9e5a32a
pradeep <pradeep8b0@gmail.com>
2023-08-08 23:46:00 +0530

    patch

--- src/router/protected-route/index.ts
+++ src/router/protected-route/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 const protectedRoute = express.Router();
 protectedRoute.get("/", async (req: Request, res: Response) => {
-  res.send(req.body);
+  res.send({ id: req.body.id as string }); //this id can excess any information of user
 });
 export { protectedRoute };
